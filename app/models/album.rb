28031bb9b9a7ab7fca596483acf082042c7ca6f7
bill-jia <bill.jiazy@gmail.com>
2015-08-22 23:15:13 -0400

    Add producer collector and redo artist collector
    Artist collector is not actually redundant because it prevents
    duplicates.

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,7 +5,14 @@ class Album < ActiveRecord::Base
 	accepts_nested_attributes_for :tracks, allow_destroy: true
 	accepts_nested_attributes_for :producers, allow_destroy: true
 
+	before_save :get_producers
 	after_create :get_artists
+
+	def get_producers
+		self.producers = self.producers.collect do |producer|
+			Producer.create_with(role: producer.role, bio: producer.bio).find_or_create_by(name: producer.name, class_year: producer.class_year)
+		end
+	end
 
 	def get_artists
 		self.artists = Array.new
