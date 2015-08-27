8660acce90bd0a413e2c9b9ccff3c9ef56710a3b
bill-jia <bill.jiazy@gmail.com>
2015-08-27 19:26:30 -0400

    Add ordering of track artists by name

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
   belongs_to :album, counter_cache: true
-  has_and_belongs_to_many :artists, counter_cache: true
+  has_and_belongs_to_many :artists, -> {order "name, class_year"}
   accepts_nested_attributes_for :artists
   mount_uploader :audio, AudioUploader
 
