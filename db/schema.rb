1647a4811d3da9a176fcb839405aebe94cb2e2fe
bill-jia <bill.jiazy@gmail.com>
2015-09-03 14:44:23 -0400

    Add devise exec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903183544) do
+ActiveRecord::Schema.define(version: 20150903184341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,35 @@ ActiveRecord::Schema.define(version: 20150903183544) do
     t.integer "artist_id", null: false
     t.integer "track_id",  null: false
   end
+
+  create_table "execs", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,       null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "email"
+    t.json     "tokens"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "execs", ["email"], name: "index_execs_on_email", using: :btree
+  add_index "execs", ["reset_password_token"], name: "index_execs_on_reset_password_token", unique: true, using: :btree
+  add_index "execs", ["uid", "provider"], name: "index_execs_on_uid_and_provider", unique: true, using: :btree
 
   create_table "producers", force: :cascade do |t|
     t.string   "name"
