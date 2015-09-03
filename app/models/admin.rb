6c8faf3f9c89b1fd1e897f7b74d4b8158fb5c4ee
bill-jia <bill.jiazy@gmail.com>
2015-09-03 14:43:16 -0400

    Add admin user

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,7 @@
-class User < ActiveRecord::Base
+class Admin < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-  before_save -> do
-    self.uid = SecureRandom.uuid
-    skip_confirmation!
-  end  
 end
