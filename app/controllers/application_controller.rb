0ba1ca1636526520e5d06feb39c5c41841e1d71e
bill-jia <bill.jiazy@gmail.com>
2015-09-04 11:22:33 -0400

    Add sanitized params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:account_update) << :name, :role, :user_role
   def artist_strong_delete(artist, delete_associated_tracks)
     if delete_associated_tracks
       artist.tracks.each do |track|
